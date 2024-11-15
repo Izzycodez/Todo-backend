12b53fe75dceb4bd037824a0341b796fed3b29f8
Malachy Isreal <izzymalsung0@gmail.com>
2024-11-15 03:51:38 +0100

    improved cors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3100);
   app.enableCors({
-    origin: '*', //'http://localhost:8081', // or
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  await app.listen(3100);
 }
 bootstrap();
